7a79567521417165582818c50f760a8acc43d8bf
Ivan Pérez-Montes Atserà <ivanpmak@gmail.com>
2025-06-02 17:29:49 +0200

    Creación de modelo parking y endpoints

---
+++ Proyecto34/src/main/java/com/xxxiv/dto/ParkingDTO.java
@@ -0,0 +1,14 @@
+package com.xxxiv.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class ParkingDTO {
+    private Integer id;
+    private String name;
+    private Integer capacity;
+}
