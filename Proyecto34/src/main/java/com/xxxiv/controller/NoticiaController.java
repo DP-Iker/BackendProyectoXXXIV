28fcc93b208e96bb17765e8accbb727c008915de
Iker Diaz <diazp.iker@gmail.com>
2025-06-05 12:31:19 +0200

    Correcion POST noticias

--- Proyecto34/src/main/java/com/xxxiv/controller/NoticiaController.java
+++ Proyecto34/src/main/java/com/xxxiv/controller/NoticiaController.java
@@ -21,6 +21,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -76,7 +77,7 @@ public class NoticiaController {
 		}
 	}
 
-	@PostMapping
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	@PreAuthorize("hasRole('ADMIN')")
 	@SecurityRequirement(name = "bearerAuth")
 	public ResponseEntity<Noticia> crear(
